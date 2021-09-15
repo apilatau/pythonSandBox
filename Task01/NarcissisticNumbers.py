146af98bcf37101ef667793f27263ec13cd6ea8b
WAW0059\apilatov <a.pilatau@gmail.com>
2021-09-15 12:38:08 +0200

    Task1 - clean code

--- Task01/NarcissisticNumbers.py
+++ Task01/NarcissisticNumbers.py
@@ -1,32 +1,33 @@
 import os,sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main():
-    print_NarcissisticNumbers(1,1000)
+    print_narcissistic_numbers(1,1001)
 
-def print_NarcissisticNumbers(minNumber,maxNumber):
-    input_list = list(range(minNumber,maxNumber,1))
+
+def print_narcissistic_numbers(minNumber,maxNumber):
+    input_list = list(range(minNumber,maxNumber))
     for input_Item in input_list:
         if input_Item == narcissistic_function(input_Item):
-            print(input_Item, end =" ")
+            print(input_Item, end=" ")
+
 
 def n_of_digits(num):
     i = 0
     while num > 0:
         num //= 10
         i+=1
-
     return i
+
 
 def narcissistic_function(num):
     i = n_of_digits(num)
-    s = 0
-    
+    s = 0    
     while num > 0:
         digit = num % 10
         num //= 10
         s += pow(digit, i)        
     return s
+
 
 if __name__=="__main__":
     main()
