1b3b3e042fb5f4a9e91e28cfdc34d90c3bd7bd24
WAW0059\apilatov <a.pilatau@gmail.com>
2021-09-23 03:18:35 +0200

    task02 clean code

--- Task02/InputOutputOperation.py
+++ Task02/InputOutputOperation.py
@@ -2,22 +2,21 @@ import math
 
 
 def menu():
-    while(True):
-        try:
-           print("Menu:")
-           print("1. Calculate triangle area by base and height")
-           print("2. Calculate triangle area by 2 sides and angle between them")
-           print("3. Exit")
-           print("Enter menu number:")
-           menu_number = int(input())
+    while True:
+        print("Menu:")
+        print("1. Calculate triangle area by base and height")
+        print("2. Calculate triangle area by 2 sides and angle between them")
+        print("3. Exit")        
+        try:           
+           menu_number = int(input("Enter menu number: "))
         except:
             print(f"You typed a character {menu_number} which could not be converted into integer")
                 
-        if(menu_number ==1):
+        if menu_number == 1:
             Area = area_by_base_height()
-        elif(menu_number == 2):
+        elif menu_number == 2:
             Area = area_by_2sides_angle()
-        elif(menu_number == 3):
+        elif menu_number == 3:
             break
         print(f"Area is: {Area}")
     
@@ -25,10 +24,9 @@ def menu():
 
 
 def area_by_base_height():
-    try:
-        print("Enter base and height:")
-        parameters = input()
-        base,height = map(int,parameters.split(' '))
+    try:        
+        parameters = input("Enter base and height: ")
+        base, height = map(int, parameters.split(' '))
     except:
         print(f"You typed a character {parameters} which could not be converted into integer")
 
