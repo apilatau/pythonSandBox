917f1fba23fcb5cc5fe1a9b53cc36326451d0e9e
WAW0059\apilatov <a.pilatau@gmail.com>
2021-10-01 17:03:42 +0200

    Task04 clean code

--- Task04/recursion.py
+++ Task04/recursion.py
@@ -10,35 +10,19 @@ tree = {
     }
 flat_tree = [1, 2, 3]
 
-list_result = list()
 
-def get_key_by_index(dictionary_item, index):
-    var = dictionary_item.keys()
-    return list(var)[index]
+def recursion(items):
+    if isinstance(items,dict):
+        collector = list() 
+        for value in items.values():
+            collector.extend(recursion(value))
+    else:
+        return items
+    return collector
 
-def remove_current_item_dictionary(item_tree):
-    key_for_removing = get_key_by_index(item_tree,0)
-    del item_tree[key_for_removing]
-    return item_tree
-
-def recursion(item):
-   if isinstance(item, list):
-       print(item)
-       return item
-
-   tree_values = item.values()   
-   
-   try: 
-    for values in tree_values:
-       if not isinstance(values,list):
-           values.update(remove_current_item_dictionary(item))
-           recursion(values)
-       else:           
-           [list_result.append(list_item) for list_item in values]
-    print(list_result)
-   except:
-    return list_result
 
 expected_result_tree = [1, 2, 3, 31, 5, 31, 7, 8, 9]
+print(recursion(tree))
+print(recursion(flat_tree))
 assert recursion(tree) == expected_result_tree,"incorrect tree plot"
 assert recursion(flat_tree) == flat_tree,"incorrect tree plot"
