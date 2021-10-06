bc6f538e1a9d9ebfc4bdb170706120c13f6fb1a8
WAW0059\apilatov <a.pilatau@gmail.com>
2021-10-06 02:08:19 +0200

    clean code

--- Task05/args_kwargs.py
+++ Task05/args_kwargs.py
@@ -11,6 +11,9 @@ print(create_user("John", "Doe"))
 print(create_user("Bill", "Gates", age=65))
 print(create_user("Marie", "Curie", age=66, occupation="physicist", won_nobel=True))
 
-assert create_user("John", "Doe") == {'name': 'John', 'surname': 'Doe', 'age': 42, 'extra':{}}, "invalid dictionary build"
-assert create_user("Bill", "Gates", age=65) == {'name': 'Bill', 'surname': 'Gates', 'age': 65, 'extra':{}}, "invalid dictionary build"
-assert create_user("Marie", "Curie", age=66, occupation="physicist", won_nobel=True) == {'name': 'Marie', 'surname': 'Curie', 'age': 66, 'extra': {'occupation': 'physicist', 'won_nobel': True}},"invalid dictionary build" 
+assert create_user("John", "Doe") == \
+    {'name': 'John', 'surname': 'Doe', 'age': 42, 'extra':{}}, "invalid dictionary build"
+assert create_user("Bill", "Gates", age=65) == \
+    {'name': 'Bill', 'surname': 'Gates', 'age': 65, 'extra':{}}, "invalid dictionary build"
+assert create_user("Marie", "Curie", age=66, occupation="physicist", won_nobel=True) == {'name': 'Marie', 'surname': 'Curie', 'age': 66, 'extra': {'occupation': 'physicist', 'won_nobel': True}},\
+    "invalid dictionary build" 
